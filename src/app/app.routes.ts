6318837fdd3909f3d9bb40266242d1b0c4a207c6
Sahil bajaj <sahilbajaj0941@gmail.com>
2025-04-09 18:46:36 +0530

    woho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { errorContext } from 'rxjs/internal/util/errorContext';
+import { ErrorComponent } from './pages/error/error.component';
+import { LoginformComponent } from './pages/loginform/loginform.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+  path:'',
+  component:HomeComponent
+ },
+    {
+      path:"about",
+      component:AboutComponent
+    },
+    {
+      path:"contact",
+      component:ContactComponent
+    },
+    {
+      path:"services",
+      component:ServicesComponent
+    },
+    {
+      path:"login",
+      component:LoginformComponent
+    },
+    {
+      path:"**",
+      component:ErrorComponent
+    }
+    
+];
---
+++ src/app/pages/loginform/loginform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginform',
+  imports: [],
+  templateUrl: './loginform.component.html',
+  styleUrl: './loginform.component.scss'
+})
+export class LoginformComponent {
+
+}
