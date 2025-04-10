fa7007313a384d176965bf8118d1627d7c071415
Sahil bajaj <sahilbajaj0941@gmail.com>
2025-04-10 19:45:24 +0530

    gh

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  imageIndex = 0;
-  images: string[] = [
-    'bg1.jpeg',
-    'bg2.jpeg',
-    'bg3.jpeg'
-  ];
+export class HomeComponent {
 
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
-
-  ngOnInit(): void {
-    this.changeBackground();
-    setInterval(() => this.changeBackground(), 5000);
-  }
-
-  changeBackground() {
-    const imageUrl = `url(${this.images[this.imageIndex]})`;
-    const container = this.el.nativeElement.querySelector('.home-container');
-    this.renderer.setStyle(container, 'background-image', imageUrl);
-    this.imageIndex = (this.imageIndex + 1) % this.images.length;
-  }
 }
